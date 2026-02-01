e60702f281646fc0226f6c7ffaf480b8a555f01f
Sambeet Sekhar Panda <sambeetpanda507@gmail.com>
2026-02-01 08:41:06 +0530

    Update 002_enable_fuzzy_search.sql

--- migrations/002_enable_fuzzy_search.sql
+++ migrations/002_enable_fuzzy_search.sql
@@ -1,2 +1,9 @@
 -- pg_trgm is used to find similarity using levenstine algorithm
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+-- Create GIST index for fuzzy search on title and description
+CREATE INDEX IF NOT EXISTS IDX_NEWS_FUZZY_SEARCH ON NEWS USING GIST (
+	(
+		COALESCE(TITLE, '') || ' ' || COALESCE(DESCRIPTION, '')
+	) GIST_TRGM_OPS
+);
